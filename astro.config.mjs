ccf8ed85e478914ab2e0a81b78c512d5ee629f9e
Juan Cruz Luna <juancruzluna@Juans-MacBook-Air.local>
2026-03-30 11:46:49 +1300

    Configure custom domain nzpublicholidays.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juancruzlunatech.github.io',
-  base: '/Public_Holidays_NZ',
+  site: 'https://nzpublicholidays.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
