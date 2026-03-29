0bc0fd79850cb56aa2e63843cb8ffa4c05947d52
Juan Cruz Luna <juancruzluna@Juans-MacBook-Air.local>
2026-03-30 12:15:13 +1300

    Add sitemap and robots.txt for Google indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nzpublicholidays.com',
   base: '/',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
